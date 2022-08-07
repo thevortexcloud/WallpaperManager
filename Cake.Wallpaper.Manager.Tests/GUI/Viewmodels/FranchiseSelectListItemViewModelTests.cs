be626174a61dba0e6718a5148171c856dd643d9b
Cake <null@localhost>
2022-08-07 18:23:28 +1000

    Made the selected franchise test name less terrible and added an additional test to check if the model was initalised correctly

--- Cake.Wallpaper.Manager.Tests/GUI/Viewmodels/FranchiseSelectListItemViewModelTests.cs
+++ Cake.Wallpaper.Manager.Tests/GUI/Viewmodels/FranchiseSelectListItemViewModelTests.cs
@@ -1,14 +1,34 @@
 using Cake.Wallpaper.Manager.Core.Models;
 using Cake.Wallpaper.Manager.GUI.ViewModels;
+using Microsoft.Reactive.Testing;
+using ReactiveUI.Testing;
 using Xunit;
 
 namespace Cake.Wallpaper.Manager.Tests.Core.GUI.Viewmodels {
     public sealed class FranchiseSelectListItemViewModelTests {
         [Fact]
-        public void SelectedSet() {
+        public void SelectedSetOnCreation() {
             var model = new FranchiseSelectListItemViewModel(new Franchise(), true);
 
             Assert.True(model.Selected);
+        }
+
+        [Fact]
+        public void ViewModelInitialisesCorrectlyFromFranchise() {
+            new TestScheduler().With((scheduler) => {
+                var franchise = new Franchise() {
+                    Depth = 1,
+                    Name = "Blah",
+                    ID = 23,
+                    ParentID = 10
+                };
+                var model = new FranchiseSelectListItemViewModel(franchise, true);
+
+                Assert.Equal(franchise.Name, model.Name);
+                Assert.Equal(franchise.Depth, model.Depth);
+                Assert.Equal(franchise.ID, model.ID);
+                Assert.Equal(franchise.ParentID, model.ParentID);
+            });
         }
     }
 }
